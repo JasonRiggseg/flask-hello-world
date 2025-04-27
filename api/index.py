7d3ee20fbf0743b3b09c6dcf102722e75fd00ed9
JasonRiggseg <63678761+JasonRiggseg@users.noreply.github.com>
2025-04-27 23:36:33 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/greet', methods=['GET'])
+def greet():
 
-@app.route('/about')
-def about():
-    return 'About'
+    name = request.args.get('name')
+
+    if name:
+        return jsonify(message=f'Hello, {name}!')
+    else:
+        return jsonify({"error": "Please pecify a name in the 'name' query parameter."}), 400
+    
+if __name__ == '__main__':
+    app.run(debug=True)    
